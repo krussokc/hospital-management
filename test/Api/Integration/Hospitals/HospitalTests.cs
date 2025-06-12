da9ba044341034de21028084ace55686ef82d5ea
Kris Russell <krussokc@gmail.com>
2025-06-12 12:06:28 -0500

    just rounding out basic webappfactory tests

--- test/Api/Integration/Hospitals/HospitalTests.cs
+++ test/Api/Integration/Hospitals/HospitalTests.cs
@@ -6,6 +6,8 @@ using Trio.HospitalManagement.Data.Mssql;
 using Xunit.Abstractions;
 using Trio.HospitalManagement.Api.Tests.Common.Extensions;
 using Shouldly;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 namespace Trio.HospitalManagement.Api.Tests.Integration.Workflows;
 
@@ -24,11 +26,47 @@ public class HospitalTests : WebApplicationTestBase<HospitalDbContext>
 
         var hospitalsUrl = "hospitals";
 
+        // create
         var createHospitalRequest = new CreateHospitalRequest { Name = "Test Hospital" };
         var createHospitalResult = await client.Post<HospitalDto>(hospitalsUrl, createHospitalRequest);
-        var hospital = createHospitalResult.Response.Data;
+        var newHospital = createHospitalResult.Response.Data;
+
+        newHospital.ShouldNotBeNull();
+        newHospital.Name.ShouldBe(createHospitalRequest.Name);
+
+        // getall
+        var hospitalsResult = await client.Get<List<HospitalDto>>(hospitalsUrl);
+        var hospitals = hospitalsResult.Response.Data;
+
+        newHospital.ShouldNotBeNull();
+        hospitals.ShouldNotBeEmpty();
+        hospitals.Count.ShouldBe(1);
+        hospitals[0].Id.ShouldBe(newHospital.Id);
+        hospitals[0].Name.ShouldBe(newHospital.Name);
+
+        var singleHospitalUrl = $"{hospitalsUrl}/{newHospital.Id}";
+
+        // get
+        var hospitalResult = await client.Get<HospitalDto>(singleHospitalUrl);
+        var hospital = hospitalResult.Response.Data;
 
         hospital.ShouldNotBeNull();
+        hospital.Id.ShouldBe(newHospital.Id);
+        hospital.Name.ShouldBe(newHospital.Name);
+
+        // update
+        var updateHospitalRequest = new UpdateHospitalRequest { Id = hospital.Id, Data = new HospitalDto { Id = hospital.Id, Name = "New Name" } };
+        var updateHospitalResult = await client.Put<HospitalDto>(singleHospitalUrl, updateHospitalRequest);
+        var updatedHospital = updateHospitalResult.Response.Data;
+
+        updatedHospital.ShouldNotBeNull();
+        updatedHospital.Name.ShouldBe(updateHospitalRequest.Data.Name);
+
+        // delete
+        var deleteHospitalResult = await client.Delete<bool>(singleHospitalUrl);
+        var deletionResult = deleteHospitalResult.Response.Data;
+
+        deletionResult.ShouldBe(true);
 
         //TestOutput.WriteLine(createHospitalResult.Content);
     }
